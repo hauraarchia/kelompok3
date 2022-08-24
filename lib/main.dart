9a4b4fcea880f194bccc118e23fc6e6a79d52f35
DELL <kakabuleng@gmail.com>
2022-08-24 11:28:49 +0700

    ubah title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Regist',
+      title: 'Regist 12',
       theme: ThemeData(
         // This is the theme of your application.
         //
