1ca4669df8cc320eca590e2d0a50ed7947151b9e
DELL <kakabuleng@gmail.com>
2022-09-06 13:51:38 +0700

    feat-regist

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './launcher.dart';
+import './.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LauncherPage(),
+      home: regist(),
     );
   }
 }
